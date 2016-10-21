e3c29a4917681b1e745781bded525e7beb0a54aa
kcgenius <carsonkyle833@gmail.com>
2016-10-21 12:19:36 +0530

    Stateless functions

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+/*
 class App extends React.Component {
   render(){
     return <div>Hi</div>
   }
 }
+*/
+
+const App = () => <h1>Hello Guys</h1>
 
 export default App
